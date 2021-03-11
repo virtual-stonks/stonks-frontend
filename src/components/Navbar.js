196887be73fdee9b69c8479389e54a396f12088e
veedee <43088920+veedee2000@users.noreply.github.com>
2021-03-11 19:46:06 +0530

    navbar v1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,20 +6,43 @@ function Navbar(props) {
 
     return (
         <div>
-            <nav className="navbar navbar-light bg-light">
-                <Link className="navbar-brand" to="/">
-                    Dashboard
-                </Link>
-                <Link className="navbar-brand" to="/">
-                    Stocks
-                </Link>
-                <Link className="navbar-brand" to="/">
-                    Holdings
-                </Link>
-                <Link className="navbar-brand" to="/">
-                    Orders
-                </Link>
+            <nav className="navbar navbar-expand-lg navbar-light" style={{backgroundColor:"wheat"}}>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="true" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon" />
+                </button>
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+                    <ul className="navbar-nav row">
+                        <li className="nav-item col">
+                            <Link className="navbar-brand" to="/">
+                                Profile
+                            </Link>
+                        </li>
+                        <li className="nav-item col">
+                            <Link className="navbar-brand" to="/">
+                                Dashboard
+                            </Link>
+                        </li>
+                        <li className="nav-item col">
+                            <Link className="navbar-brand" to="/">
+                                Market
+                            </Link>
+                        </li>
+                        <li className="nav-item col">
+                            <Link className="navbar-brand" to="/">
+                                Holdings
+                            </Link>
+                        </li>
+                        <li className="nav-item col">
+                            <Link className="navbar-brand" to="/">
+                                Wallet
+                            </Link>
+                        </li>
+
+                    </ul>
+                </div>
+                <h7 className="mt-2" style={{fontFamily:"fantasy"}}>VIRTUAL STONKS</h7>
             </nav>
+
         </div>
     )
 }
