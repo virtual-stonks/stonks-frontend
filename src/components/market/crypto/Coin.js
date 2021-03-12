9defa664700ccabd2d48327eb4196c8a5cc4eed2
Sabuj Jana <sabujlovescoding@gmail.com>
2021-03-12 15:57:39 +0000

    added crypto market basics

--- src/components/market/crypto/Coin.js
+++ src/components/market/crypto/Coin.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import numeral from "numeral"
-const Coin = ({ name, price, symbol, image, priceChange, marketCap }) => {
+const Coin = ({ name, price, symbol, image, priceChange, marketCap, low_24h, high_24h, total_volume }) => {
     return (
         <>
             <tr>
@@ -9,11 +9,17 @@ const Coin = ({ name, price, symbol, image, priceChange, marketCap }) => {
                 <td>{symbol.toUpperCase()}</td>
                 <td>$ {numeral(marketCap).format("0.0a")}</td>
                 <td>$ {numeral(price).format("0.0a")}</td>
+                <td>$ {numeral(low_24h).format("0.0a")}</td>
+                <td>$ {numeral(high_24h).format("0.0a")}</td>
                 <td
                     style={{ color: `${priceChange < 0 ? "red" : "green"}` }}
                 >
                     {priceChange.toFixed(2)}
+                    <i
+                        className={`${priceChange < 0 ? "fa fa-fw fa-sort-desc text-danger" : "fa fa-fw fa-sort-asc text-success"}`}
+                    />
                 </td>
+                <td> {numeral(total_volume).format("0.0a")}</td>
             </tr>
         </>
     )
--- src/components/market/crypto/Crypto.js
+++ src/components/market/crypto/Crypto.js
@@ -17,7 +17,7 @@ const Crypto = () => {
     useEffect(() => {
         axios
             .get(
-                'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+                'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=1000&page=1&sparkline=false'
             )
             .then(res => {
                 setCrypto(res.data);
@@ -43,87 +43,130 @@ const Crypto = () => {
     }
 
     return (
-        <div className="row container">
-            <div className="col-sm-6">
-                <h1 className='crypto-text'>Search a Crypto</h1>
-                <form>
-                    <div className="form-group">
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="exampleInputEmail1"
-                            aria-describedby="emailHelp"
-                            placeholder="BTC, ETH..."
-                        />
-                    </div>
-                </form>
-                <table className="table table-striped table-dark table-responsive">
-                    <thead>
-                        <tr>
-                            <th scope="col">Coin</th>
-                            <th scope="col">
-                                Name
-                                <i
-                                    className="fa fa-fw fa-sort"
-                                    onClick={() => sortBy("name")}
-                                    style={{ cursor: "pointer" }}
-                                />
-                            </th>
-                            <th scope="col">
-                                Sym
-                                <i
-                                    className="fa fa-fw fa-sort"
-                                    onClick={() => sortBy("symbol")}
-                                    style={{ cursor: "pointer" }}
-                                />
-                            </th>
-                            <th scope="col">
-                                MC
-                                <i
-                                    className="fa fa-fw fa-sort"
-                                    onClick={() => sortBy("market_cap")}
-                                    style={{ cursor: "pointer" }}
-                                />
-                            </th>
-                            <th scope="col">
-                                LTP
-                                <i
-                                    className="fa fa-fw fa-sort"
-                                    onClick={() => sortBy("current_price")}
-                                    style={{ cursor: "pointer" }}
-                                />
-                            </th>
-                            <th scope="col">
-                                %
-                                <i
-                                    className="fa fa-fw fa-sort"
-                                    onClick={() => sortBy("price_change_percentage_24h")}
-                                    style={{ cursor: "pointer" }}
-                                />
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {crypto.map(coin => {
-                            return (
-                                <Coin
-                                    key={coin.id}
-                                    name={coin.name}
-                                    price={coin.current_price}
-                                    symbol={coin.symbol}
-                                    image={coin.image}
-                                    priceChange={coin.price_change_percentage_24h}
-                                    marketCap={coin.market_cap}
-                                />
-                            )
-                        })}
+        <div className="container">
 
-                    </tbody>
-                </table>
-            </div >
-            <div className="col-sm-6">
+            <h1 className='crypto-text'>Search a Crypto</h1>
+            <form>
+                <div className="form-group">
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="exampleInputEmail1"
+                        aria-describedby="emailHelp"
+                        placeholder="BTC, ETH..."
+                    />
+                </div>
+            </form>
+            <table className="table table-striped table-dark table-responsive">
+                <thead>
+                    <tr>
+                        <th scope="col"
+                            style={{ color: "#00ff00" }}
+                        >Coin</th>
+                        <th scope="col"
+                            style={{ color: "#00ff00" }}
+                        >
+                            Name
+                                    <i
+                                className="fa fa-fw fa-sort"
+                                onClick={() => sortBy("name")}
+                                style={{ cursor: "pointer" }
+                                }
+                            />
+                        </th>
+                        <th scope="col"
+                            style={{ color: "#00ff00" }}
+                        >
+                            Sym
+                                <i
+                                className="fa fa-fw fa-sort"
+                                onClick={() => sortBy("symbol")}
+                                style={{ cursor: "pointer" }}
+                            />
+                        </th>
+                        <th scope="col"
+                            style={{ color: "#00ff00" }}
+                        >
+                            MC
+                                <i
+                                className="fa fa-fw fa-sort"
+                                onClick={() => sortBy("market_cap")}
+                                style={{ cursor: "pointer" }}
+                            />
+                        </th>
+                        <th scope="col"
+                            style={{ color: "#00ff00" }}
+                        >
+                            Price
+                                <i
+                                className="fa fa-fw fa-sort"
+                                onClick={() => sortBy("current_price")}
+                                style={{ cursor: "pointer" }}
+                            />
+                        </th>
+                        <th scope="col"
+                            style={{ color: "#00ff00" }}
+                        >
+                            24h L
+                                <i
+                                className="fa fa-fw fa-sort"
+                                onClick={() => sortBy("low_24h")}
+                                style={{ cursor: "pointer" }}
+                            />
+                        </th>
+                        <th scope="col"
+                            style={{ color: "#00ff00" }}
+                        >
+                            24h H
+                                <i
+                                className="fa fa-fw fa-sort"
+                                onClick={() => sortBy("high_24h")}
+                                style={{ cursor: "pointer" }}
+                            />
+                        </th>
+                        <th scope="col"
+                            style={{ color: "#00ff00" }}
+                        >
+                            24h change
+                                <i
+                                className="fa fa-fw fa-sort"
+                                onClick={() => sortBy("price_change_percentage_24h")}
+                                style={{ cursor: "pointer" }}
+                            />
+                        </th>
+                        <th scope="col"
+                            style={{ color: "#00ff00" }}
+                        >
+                            Traded Volume
+                                <i
+                                className="fa fa-fw fa-sort"
+                                onClick={() => sortBy("total_volume")}
+                                style={{ cursor: "pointer" }}
+                            />
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {crypto.map(coin => {
+                        return (
+                            <Coin
+                                key={coin.id}
+                                name={coin.name}
+                                price={coin.current_price}
+                                symbol={coin.symbol}
+                                image={coin.image}
+                                priceChange={coin.price_change_percentage_24h}
+                                marketCap={coin.market_cap}
+                                low_24h={coin.low_24h}
+                                high_24h={coin.high_24h}
+                                total_volume={coin.total_volume}
+                            />
+                        )
+                    })}
 
-            </div>
+                </tbody>
+            </table>
+
         </div >
     )
 }
