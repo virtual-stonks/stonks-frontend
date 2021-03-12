a77c00340e0e7726615f2ae5017f0518913831d2
Dipangshu Roy <57279309+Droyder7@users.noreply.github.com>
2021-03-12 21:55:11 +0530

    Added Signup & Signin Pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
-import React from 'react'
-import { Route } from "react-router-dom"
+import React from 'react';
+import { Route } from 'react-router-dom';
 
-import Navbar from './components/Navbar'
-import Dashboard from './components/Dashboard'
+import Navbar from './components/Navbar';
+import Dashboard from './components/Dashboard';
+import Signin from './components/auth/Signin';
+import Signup from './components/auth/Signup';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Route exact path="/" component={Dashboard} />
+      <Route exact path="/signin" component={Signin} />
+      <Route exact path="/signup" component={Signup} />
     </div>
-  )
+  );
 }
 
 export default App;
