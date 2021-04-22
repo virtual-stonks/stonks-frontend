0d8f6040229b678cafde3774fbdb70b13755f685
Sabuj Jana <sabujlovescoding@gmail.com>
2021-04-22 08:01:03 +0000

    live holdings

--- src/components/holdings/Analysis.js
+++ src/components/holdings/Analysis.js
@@ -7,19 +7,19 @@ const Analysis = ({analysisdata}) => {
         <Row>
             <Col sm="4">
                 <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
-                    <CardTitle tag="h5">{total_inv.toFixed(2)}</CardTitle>                    
+                    <CardTitle tag="h5">{total_inv.toFixed(2)}</CardTitle>                                                        
                     <Button>Total Investment</Button>
                 </Card>
             </Col>
             <Col sm="4">
                 <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
-                    <CardTitle tag="h5">{cur_val.toFixed(2)}</CardTitle>                    
+                    <CardTitle tag="h5" id={"holdings_currval_total"}>{cur_val.toFixed(2)}</CardTitle>                    
                     <Button>Current Value</Button>
                 </Card>
             </Col>
             <Col sm="4">
                 <Card body inverse color={Number(pl) > 0 ? "success": "danger" }>
-                    <CardTitle tag="h5">{Number(pl) > 0 ? pl.toFixed(2): -pl.toFixed(2)}</CardTitle>                    
+                    <CardTitle tag="h5" id={"holdings_pl_total"}>{Number(pl) > 0 ? pl.toFixed(2): -pl.toFixed(2)}</CardTitle>                    
                     <Button>Profit/Loss</Button>
                 </Card>
             </Col>
